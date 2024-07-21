9958c549b31a53d08e21166d0f1fc613d4317d0b
gityashk <m.yashk6+github.com>
2024-07-22 00:14:56 +0530

    added link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <>
             <footer className="flex text-sm font-serif text-[#cbd5e1] justify-center mt-12 py-1 px-2">
-                <p>Made with&nbsp;</p><img src={heart} /><p>&nbsp;by Yash K.</p>
+                <p>Made with&nbsp;</p><img src={heart} /><p>&nbsp;by&nbsp;</p><a className="hover:underline hover:text-slate-400" href="https://www.linkedin.com/in/m-yashk/" target="_blank">Yash K.</a>
             </footer>
         </>
     )
